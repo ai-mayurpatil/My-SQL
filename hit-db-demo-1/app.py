e02846fa25030e9cba7bd199c15b54eaa8a0d031
ai-mayurpatil <ai.mayurpatil@gmail.com>
2024-12-16 10:50:44 +0530

    code connect to database done

---
+++ hit-db-demo-1/app.py
@@ -0,0 +1,27 @@
+import sys
+from dbhelper import DBhelper
+class Flipkart:
+    def __init__(self):
+        # connect to the database
+        self.db = DBhelper()
+        self.menu()
+
+    def menu(self):
+
+        user_input = input("""
+        1. Enter 1 to register
+        2. Enter 2 to login
+        3. Anything else to leave     
+        """)
+
+        if user_input == "1":
+            self.register()
+        elif user_input == "2":
+            self.login()
+        else:
+            sys.exit(1000)
+    
+    def register(self):
+        pass
+
+obj = Flipkart()
--- hit-db-demo-1/dbhelper.py
+++ hit-db-demo-1/dbhelper.py
@@ -1,1 +1,16 @@
 import mysql.connector
+import sys
+
+class DBhelper:
+
+    def __init__(self):
+        try:
+            self.conn = mysql.connector.connect(host="localhost", user="root", password="", database="hit-db-demo")
+            self.mycursor = self.conn.cursor()
+        except:
+            print("Some erroe occured Could not connect to database")
+            sys.exit(0)
+            
+        else:
+            print("Connected to database")
+            
